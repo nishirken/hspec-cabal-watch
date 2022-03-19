16a684d215f40224117840a7bc7be05aeaeee355
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2022-03-20 00:37:47 +0400

    add watch cmd

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,32 @@
+{-# LANGUAGE OverloadedStrings  #-}
+
 module Main where
 
+import qualified Shelly as Sh
+import qualified Console.Options as Cli
+import Data.Text (pack, unpack)
+import Data.Maybe (fromMaybe)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = Cli.defaultMain $ do
+  cabalArgs <- Cli.flagParam (Cli.FlagLong "cabal-args") $ Cli.FlagOptional "" Right
+  match <- Cli.flagParam (Cli.FlagLong "match") $ Cli.FlagOptional "" Right
+  Cli.action $ \toParam -> Sh.shelly $ do
+    hasGit <- Sh.test_e ".gitignore"
+    let
+      lsCmd = if hasGit then "git ls-files" else "ls -R"
+      withQuotes str = "\"" <> str <> "\""
+      grepRegex = withQuotes "\\.\\(hs\\|cabal\\)$"
+      watchFiles = lsCmd <> " | grep " <> grepRegex
+      matchArgs = fromMaybe "" $ do
+        cmd' <- toParam match
+        pure $ "--test-option=--match --test-option=" <> withQuotes cmd'
+      cabalCmd = unwords
+        [ "cabal v2-test"
+        , "--test-show-details=direct"
+        , "--disable-optimization"
+        , matchArgs
+        , fromMaybe "" $ toParam cabalArgs
+        ]
+    Sh.bash_ (watchFiles <> " | " <> "entr -s " <> withQuotes cabalCmd) []
+
--- flake.nix
+++ flake.nix
@@ -8,7 +8,9 @@
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
     let
-      packageName = "watch";
+      packageName = "hspec-cabal-watch";
+      execName = "hcw";
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
       overlays = [ haskellNix.overlay
         (final: prev: {
           # This overlay adds our project to pkgs
@@ -23,16 +25,20 @@
                   haskell-language-server = "latest";
                   ormolu = "0.1.4.1";
                 };
-                buildInputs = [pkgs.haskellPackages.implicit-hie];
+                buildInputs = [pkgs.haskellPackages.implicit-hie pkgs.entr];
                 withHoogle = true;
               };
             };
-        })
-      ];
-      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+          })
+        ];
       flake = pkgs.${packageName}.flake {};
     in flake // {
       # Built by `nix build .`
-      defaultPackage = flake.packages."${packageName}:exe:${packageName}";
+      defaultPackage = (flake.packages."${packageName}:exe:${execName}".overrideAttrs (oldAttrs: {
+        buildInputs = oldAttrs.buildInputs or [] ++ [pkgs.makeWrapper];
+        postInstall = oldAttrs.postInstall or "" + ''
+          wrapProgram $out/bin/${execName} --prefix PATH : ${pkgs.lib.makeBinPath [pkgs.entr]}
+        '';
+      }));
     });
-}
+  }
